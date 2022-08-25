fc3ce7009a9bf384f31fc5ed2c3cf6b5aa847ed1
Musf1c <dmitry.m228@mail.ru>
2022-08-25 23:56:18 +0300

    Создание проекта и настройка зависимостей

---
+++ src/main/java/ru/mitrofan/film/MyFilmServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.mitrofan.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFilmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFilmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mitrofan/film/MyFilmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mitrofan.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFilmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
